fe02a27bfb838b8b51f496476d9c3c5b9ee6ee51
AMRrah <124433195+AMRrah@users.noreply.github.com>
2025-11-26 14:29:41 +0530

    Import useEffect and useRef in MessageScreen

--- src/components/screens/MessageScreen.jsx
+++ src/components/screens/MessageScreen.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import GradientButton from "../GradientButton";
 import { ArrowRight } from "lucide-react";
